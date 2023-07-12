2242555e429a6a1a6675044a503f0bdecff440cd
Pavel Husa <husa@shoptet.cz>
2023-07-12 13:12:15 +0200

    Prompt and script/style template

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 #!/usr/bin/env node
-import { input } from '@inquirer/prompts';
+import { input, confirm } from '@inquirer/prompts';
+import fs from 'fs';
+import PackageJson from '@npmcli/package-json';
 
-const answer = await input({ message: 'Enter the Addon name' });
+const addonName = await input({ message: 'Enter the Addon name' });
+const initExample = await confirm({
+    message: 'Do you want to init with example?',
+});
+const initBender = await confirm({
+    message: 'Do you want to init with Shoptet Bender?',
+});
+let remoteEshopUrl = null;
+if (initBender) {
+    remoteEshopUrl = await input({
+        message: 'Enter the eshop url',
+    });
+}
 
-console.log(answer);
+try {
+    if (initExample) {
+        fs.cpSync('./template', `./${addonName}`, { recursive: true });
+    } else {
+        fs.mkdirSync(`./${addonName}`);
+    }
+} catch (err) {
+    if (err.code !== 'EEXIST') throw err;
+}
+
+// add name and devDependecny to package.json using package-json
+/*
+    "name": "shoptet-${addonName}",
+    "devDependencies": {
+        "shp-bender": "git+https://github.com/shoptet/shoptet-bender.git"
+    }
+*/
+const path = `./${addonName}/`;
+const pkgJson = await PackageJson.load(path);
+pkgJson.update({
+    name: addonName,
+    devDependencies: {
+        ...pkgJson.content.devDependencies,
+        'shp-bender': 'git+https://github.com/shoptet/shoptet-bender.git',
+    },
+});
+if (remoteEshopUrl) {
+    pkgJson.update({
+        scripts: {
+            'dev': `shp-bender --remote ${remoteEshopUrl}`,
+        }
+    });
+}
+pkgJson.normalize()
+pkgJson.save();
